c8b2b1578c6f880688bccda74b3e9d54e6d3665e
hokkung <hokkung.leo@gmail.com>
2021-05-29 13:03:27 +0700

    init

---
+++ src/test/java/com/example/dbchangelog/DbChangelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbchangelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbChangelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
